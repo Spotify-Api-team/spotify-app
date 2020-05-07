485183f2af853d04495ed8716926c83450b7b28a
MLHLocalhost <localhost@mlh.io>
2020-05-07 13:18:39 -0400

    added cs and js direcotry

---
+++ src/main/resources/static/js/custom.js
@@ -0,0 +1,15 @@
+function myFunction(){
+    document.getElementById("frm1").submit();
+    window.open('google.com');
+    alert("Success!");
+    open("/success")
+
+    /*
+        <script>
+        function myFunction() {
+            document.getElementById("frm1").submit();
+            window.open('google.com');
+        }
+    </script>
+     */
+}
