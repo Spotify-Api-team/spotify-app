55cc5f6fa40c68c290c5b503f27105c4616c30bc
MLHLocalhost <localhost@mlh.io>
2020-05-07 13:16:24 -0400

    added cs and js direcotry

--- src/main/java/com/Jukbox/controller/testcontroller.java
+++ src/main/java/com/Jukbox/controller/testcontroller.java
@@ -1,4 +1,13 @@
 package com.Jukbox.controller;
 
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
 public class testcontroller {
+    @RequestMapping("/")
+    public String start(){
+        return "index";   //thymeleaf expects the file index to be in the templates folder
+    }
+
 }
