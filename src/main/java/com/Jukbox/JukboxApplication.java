75151ad196258af6c0bbe94c4b29258b6ab47e8a
Aaron Schaefer <ars9525@g.rit.edu>
2020-05-07 12:25:59 -0400

    First commit

---
+++ src/main/java/com/Jukbox/JukboxApplication.java
@@ -0,0 +1,13 @@
+package com.Jukbox;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JukboxApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(JukboxApplication.class, args);
+	}
+
+}
