35b7c849ac2077623a0cd1b1e3794838d8eaf6c7
Aaron Schaefer <ars9525@g.rit.edu>
2020-05-08 12:19:25 -0400

    work on test controller

--- src/main/java/com/Jukbox/controller/testcontroller.java
+++ src/main/java/com/Jukbox/controller/testcontroller.java
@@ -1,13 +1,25 @@
 package com.Jukbox.controller;
 
+import com.Jukbox.model.Member;
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
 
-@Controller
+@RestController
 public class testcontroller {
+
     @RequestMapping("/")
     public String start(){
         return "index";   //thymeleaf expects the file index to be in the templates folder
+    }
+
+    @RequestMapping("controller/test")
+    @PostMapping
+    public void addMember(Member member){
+
+        System.out.println(member.getName());
+        
     }
 
 }
--- src/main/java/com/Jukbox/model/Owner.java
+++ src/main/java/com/Jukbox/model/Owner.java
@@ -1,13 +1,28 @@
 package com.Jukbox.model;
 
+/**
+ * Owners are the users that make the rooms
+ * They likely need a spotify premium account
+ * MOST LIKELY WILL NEED TO ADD MORE TO THIS CLASS LIKE SPOTIFY CREDENTIALS/USER INFO LATER ON
+ *
+ */
 public class Owner{
 
+    //spotify username
     private String spotifyName;
 
+    /**
+     * create owner object
+     * @param spotifyName spotify username
+     */
     public Owner(String spotifyName) {
         this.spotifyName = spotifyName;
     }
 
+    /**
+     * get the spotify name
+     * @return spotify username
+     */
     public String getSpotifyName(){
 
         return spotifyName;
