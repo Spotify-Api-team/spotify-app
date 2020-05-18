9067653d1420e6132dc35042a17c8e9becc853bc
Michael Jones <mwjones@syr.edu>
2020-05-18 19:11:25 -0400

    cahnging over to js files

--- src/main/resources/static/js/login.js
+++ src/main/resources/static/js/login.js
@@ -1,75 +1,75 @@
-$(document).ready(function() {
-    $("Form.one").on("submit", function () {
+function testF() {
+    $(document).ready(function () {
+        $("Form.one").on("submit", function () {
 
-        console.log("console log working");
+            console.log("console log working");
 
-        var that = $(this), //that reference current object we are working with
-                            //url = that.attr('action'),
-                            //type = that.attr('method'),
-            data = {};
+            var that = $(this), //that reference current object we are working with
+                //url = that.attr('action'),
+                //type = that.attr('method'),
+                data = {};
 
-        that.find("[name]").each(function (index, value) {
-            var that = $(this),
-                name = that.attr("name"),
-                value = that.val();
-            data[name] = value;
+            that.find("[name]").each(function (index, value) {
+                var that = $(this),
+                    name = that.attr("name"),
+                    value = that.val();
+                data[name] = value;
+            });
+            $.ajax({
+                type: "POST",
+                contentType: "application/json",
+                url: "http://localhost:8080/createRoom",
+                data: JSON.stringify(data),
+                success: function (response) {
+                    console.log("success");
+
+                    //spotify login if post is successful
+                    var params = {
+                        response_type: "code",
+                        client_id: "b1e9cb8d5176473fb39f5e7aca4eaae9",
+                        redirect_uri: "http://localhost:8080/Room",
+                        scope: "streaming user-read-email user-read-private"
+                    };
+                    window.location ='https://accounts.spotify.com/authorize?' +
+                        $.param(params);
+                },
+                error: function (jqXhr, textStatus, errorMessage) {
+                    console.log("error" + errorMessage);
+                }
+            });
+            return false;
         });
-        $.ajax({
-            type: "POST",
-            contentType: "application/json",
-            url: "http://localhost:8080/createRoom",
-            data: JSON.stringify(data),
-            success:function(response){
-                console.log("success");
-
-                //spotify login if post is successful
-                var params = {
-                    response_type: "code",
-                    client_id: "b1e9cb8d5176473fb39f5e7aca4eaae9",
-                    redirect_uri: "http://localhost:8080/Room",
-                    scope: "streaming user-read-email user-read-private"
-                };
-                window.location.replace('https://accounts.spotify.com/authorize?' +
-                    $.param(params) );
-            },
-            error:function(jqXhr, textStatus, errorMessage){
-                console.log("error" + errorMessage);
-            }
-        });
-        return false;
     });
-});
+}
+
+    $(document).ready(function() {
+        $("Form.two").on("submit", function () {
 
 
+            var that = $(this), //that reference current object we are working with
+                //url = that.attr('action'),
+                //type = that.attr('method'),
+                data = {};
 
-$(document).ready(function() {
-    $("Form.two").on("submit", function () {
-
-
-        var that = $(this), //that reference current object we are working with
-            //url = that.attr('action'),
-            //type = that.attr('method'),
-            data = {};
-
-        that.find("[name]").each(function (index, value) {
-            var that = $(this),
-                name = that.attr("name"),
-                value = that.val();
-            data[name] = value;
+            that.find("[name]").each(function (index, value) {
+                var that = $(this),
+                    name = that.attr("name"),
+                    value = that.val();
+                data[name] = value;
+            });
+            $.ajax({
+                type: "POST",
+                contentType: "application/json",
+                url: "http://localhost:8080/joiningRoom",
+                data: JSON.stringify(data),
+                success:function(response){
+                    console.log("success");
+                    window.location.replace("http://localhost:8080/JoinRoom");
+                },
+                error:function(jqXhr, textStatus, errorMessage){
+                    console.log("error" + errorMessage);
+                }
+            });
+            return false;
         });
-        $.ajax({
-            type: "POST",
-            contentType: "application/json",
-            url: "http://localhost:8080/joiningRoom",
-            data: JSON.stringify(data),
-            success:function(response){
-                console.log("success");
-                window.location.replace("http://localhost:8080/JoinRoom");
-            },
-            error:function(jqXhr, textStatus, errorMessage){
-                console.log("error" + errorMessage);
-            }
-        });
-        return false;
     });
-});
