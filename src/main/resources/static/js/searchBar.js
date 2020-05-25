db64479ed023110048885599a265d8753f6ff353
Michael Jones <mwjones@syr.edu>
2020-05-25 15:14:29 -0400

    search bar added

--- src/main/resources/static/js/searchBar.js
+++ src/main/resources/static/js/searchBar.js
@@ -1,10 +1,12 @@
 const searchBar = document.getElementById('searchBar');
+const songList = document.getElementById('songList');
 
 searchBar.addEventListener('keyup',(e)=>{
     console.log(e.target.value);
     const val= e.target.value;
     if(val.length>1){
-    searchFunction(val);
+        searchFunction(val);
+        //songList = generateList(val);
     }
 
 });
@@ -41,12 +43,42 @@ function searchFunction(e){
         },
         success: function(response){
             console.log(response);
+            var array=[];
+            var i=0;
+            while(i<3){
+                //console.log(response.tracks.items[i].name);
+                //array = array.concat(response.track.items[i].name);
+                console.log(response.tracks.items[i]);
+                displaySongs(response.tracks.items);
+                i = i+1;
+            }
+            //console.log(response.tracks.items[0].name);
+
+
         }
 
     });
 }
 
+const displaySongs = (songs) => {
+    const htmlString = songs
+        .map((song) => {
+            return `
+            <li class="character">
+                <h2>${song.name}</h2>
+                <img src="${song.album.images[2].url}">
+            
+            </li>
+        `;
+        })
+        .join('');
+    songList.innerHTML = htmlString;
+};
 
+/*
+function displaySongs(song){
+    console.log(song.name);
+}*/
 
 
 
