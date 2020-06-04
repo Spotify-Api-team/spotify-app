38e078db1519416f040e325e3aaaaa5d246eb15f
Aaron Schaefer <ars9525@g.rit.edu>
2020-06-04 12:03:43 -0400

    added queue to memberpage aswell as ownerpage

--- src/main/resources/static/js/queueSongs.js
+++ src/main/resources/static/js/queueSongs.js
@@ -1,6 +1,5 @@
 
 var intervalID = window.setInterval(checkQueue, 1000);
-var intervalID = window.setInterval(updateQueue, 1000);
 
 
 function checkQueue(){
@@ -41,39 +40,5 @@ function sendToQueue(song){
 
 }
 
-
-function updateQueue(){
-
-    songs = null;
-
-    $.ajax({
-            type: "GET",
-            url: "http://localhost:8080/confirmedSong",
-            dataType: "json",
-            success: function(response){
-                console.log(response);
-                displayQueue(response);
-            },
-            error: function(error){
-                //console.log("error in checkqueue");
-                //console.log(error);
-            }
-    });
-
-}
-
-const displayQueue = (songs) => {
-    const htmlStringQueue = songs
-        .map((song) => {
-            return `
-            <li class="queueTrack" id="${song.id}"  >
-                <img src="${song.image.url}" style = "float left; width: 30px; height 30px;">
-                <div class="info"> ${song.name} </div>
-            </li>
-        `;
-        })
-        .join('');
-    queue.innerHTML = htmlStringQueue;
-};
 
 
