4a5fc82f55638325b070b685a92fa0a3320f7f56
Aaron Schaefer <ars9525@g.rit.edu>
2020-06-14 11:56:27 -0400

    getting tomcat war deployment to work

--- src/main/java/com/Jukbox/JukboxApplication.java
+++ src/main/java/com/Jukbox/JukboxApplication.java
@@ -2,9 +2,17 @@ package com.Jukbox;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
 
 @SpringBootApplication
-public class JukboxApplication {
+public class JukboxApplication extends SpringBootServletInitializer {
+
+	@Override
+	protected SpringApplicationBuilder configure(SpringApplicationBuilder builder) {
+		return builder.sources(JukboxApplication.class);
+	}
+
 
 	public static void main(String[] args) {
 		SpringApplication.run(JukboxApplication.class, args);
