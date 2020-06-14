c3c163ac52a5f9911293d6bf20268153594758fc
Michael Jones <mwjones@syr.edu>
2020-06-14 12:58:07 -0400

    added resouce handler trying to get css and js

---
+++ src/main/java/com/Jukbox/config/WebConfig.java
@@ -0,0 +1,13 @@
+package com.Jukbox.config;
+
+import org.springframework.boot.autoconfigure.web.servlet.WebMvcAutoConfiguration;
+import org.springframework.web.servlet.config.annotation.ResourceHandlerRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurerAdapter;
+
+public class WebConfig extends WebMvcConfigurerAdapter {
+
+    @Override
+    public void addResourceHandlers(ResourceHandlerRegistry registry) {
+        registry.addResourceHandler("/resources/static/**").addResourceLocations("/resources/static");
+    }
+}
